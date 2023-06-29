71a807e6f507f6debc7a4a94aceac91231e97bd3
jenniferkelly <jenbilyeu@yahoo.com>
2023-06-28 19:19:10 -0500

    Lesson2

---
+++ src/exercises/ch3/HashMapPractice.java
@@ -0,0 +1,13 @@
+package exercises.ch3;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class HashMapPractice {
+    public static void main(String[] args){
+        HashMap<Integer, String> Students = new HashMap<>();
+Scanner input=new Scanner(System.in);
+String newStudent;
+
+        System.out.println("Enter your students (or ENTER to finish):");
+    }
+}
